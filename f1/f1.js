7884fd8834cf9ea26d9c37fc8f4b81535b96c626
BocsKrisztian <bocskrisz@gmail.com>
2024-10-08 13:31:19 +0200

    Update f1.js

--- f1/f1.js
+++ f1/f1.js
@@ -1,7 +1,11 @@
 
 const APIAdress = 'https://archive-api.open-meteo.com/v1/archive?';
 async function f1 (lat, long, timeStr) {
-    //TODO
+        const response = await fetch(`APIAdress`)
+        const data = await response.json();
+        const max = data.daily.temperature_2m_max[0];
+        const min = data.daily.temperature_2m_min[0];
+        const avgTemp = Math.floor((max + min) / 2);
 }
 
 module.exports = f1;
